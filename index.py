fcdb1bf28518bf49d50a909f29a23786b988983b
Mayank chauhan <mctyson1795@gmail.com>
2013-10-19 15:58:20 +0530

    my done

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
-def main():
-    pass
+
 def cube_volume():
-    pass
+    side=raw_input("Enter side: ")
+    volume=s*s*s*1.0
+    print volume
 def cube_surface_area():
-    pass
-def cuboid_volume():
-    pass
-def cuboid_surface_area():
-    pass
+    side=raw_input("Enter side: ")
+    surfacearea=6.0*s*s
+    print surfacearea
+
