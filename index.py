ce9227b6a68f8e60062642738018d9b828cc2dad
vaibhav sharma <sharma.vaibhav9518@gmail.com>
2013-10-19 16:15:32 +0530

    new changes of mine

--- index.py
+++ index.py
@@ -1,5 +1,22 @@
 <<<<<<< HEAD
 
+def main():
+    print "**************MENU*******************"
+    print "1]VOLUME OF CUBE"
+    print "2]SURFACE AREA OF CUBE"
+    print "3]VOLUME OF CUBOID"
+    print "4]SURFACE AREA OF CUBOID"
+    e=int(raw_input("\n\t\tenter your choice:"))
+    if(e==1):
+        cube_volume()
+    elif(e==2):
+        cube_surface_area()
+    elif(e==3):
+        cuboid_volume()
+    else:
+        cuboid_surface_area() 
+
+
 def cube_volume():
     side=raw_input("Enter side: ")
     volume=s*s*s*1.0
